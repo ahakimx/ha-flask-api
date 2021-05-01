675687da88b6639cb2ae39f68c29709534d4a32f
ahakimx <ahakim338@gmail.com>
2021-05-01 16:52:29 +0700

    deploy apps to eks

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify(message=' Hello world!'), 200
+    return jsonify(message=' Hello world! v1'), 200
 
 if __name__ == '__main__':
     app.run()
